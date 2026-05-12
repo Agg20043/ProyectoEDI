c4eb852262a0fc43a09e1ed84b6cae62faa3c2cc
CarolusPingas <81183940+CarolusPingas@users.noreply.github.com>
2026-05-12 14:58:34 +0200

    Update GestorArtista.h

--- modelo/GestorArtista.h
+++ modelo/GestorArtista.h
@@ -1,34 +1,51 @@
+
 #ifndef GESTORARTISTA_H
 #define GESTORARTISTA_H
 
 //#define LISTA_ARTISTAS
 
 #include "Artista.h"
+#include <iostream>
+#include <string>
 
 #if defined(LISTA_ARTISTAS)
     #include "ListaDPI.h"
+
 #else
+
     #include "bstree.h"
     #include "KeyValue.h"
+
 #endif
 
+using namespace std;
+
 class GestorArtistas {
+
 private:
+
+#if defined(LISTA_ARTISTAS)
     ListaDPI<Artista*> *lArtistas;
+#else
 
     BSTree<KeyValue<string, Artista*>> *aArtistas;
-    Artista* buscarRec(BSTree<KeyValue<string, Artista*>>* nodo,
-                       const string& nombre) const;
+    Artista* buscarArb(BSTree<KeyValue<string, Artista*>>* nodo, const string& nombre) const;
 
+    void mostrarArbrec(BSTree<KeyValue<string, Artista*>>* nodo) const;
+    int contarArbrec(BSTree<KeyValue<string, Artista*>>* nodo) const;
+    void topArblrec(BSTree<KeyValue<string, Artista*>>* nodo,Artista*& top,int& maxSeguidores) const;
+    void copiarArbolrec(BSTree<KeyValue<string, Artista*>>* nodo);
+
+#endif
 
 public:
+
     GestorArtistas();
     GestorArtistas(const GestorArtistas& otro);
     ~GestorArtistas();
 
     void insertar(Artista* artista);
     int numElementos() const;
-
     void mostrarTop() const;
 
     Artista* buscar(const string& nombre) const;
