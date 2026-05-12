5514ec32ac973e1f530470b9e37c43f2d471562f
CarolusPingas <81183940+CarolusPingas@users.noreply.github.com>
2026-05-12 14:45:00 +0200

    Update GestorArtista.cpp

--- controlador/GestorArtista.cpp
+++ controlador/GestorArtista.cpp
@@ -1,14 +1,18 @@
-//
-// Created by xalva on 07/04/2026.
-//
+
 #include "../modelo/GestorArtista.h"
-#include <iostream>
+
+#if defined(LISTA_ARTISTAS)
+
+
+// En caso de usarse por Listas
 
 GestorArtistas::GestorArtistas() {
+
     lArtistas = new ListaDPI<Artista*>();
 }
 
 GestorArtistas::GestorArtistas(const GestorArtistas& otro) {
+
     lArtistas = new ListaDPI<Artista*>();
     otro.lArtistas->moverPrimero();
     while (!otro.lArtistas->alFinal()) {
@@ -19,7 +23,8 @@ GestorArtistas::GestorArtistas(const GestorArtistas& otro) {
     }
 }
 
-GestorArtistas::~GestorArtistas() { // [cite: 25]
+GestorArtistas::~GestorArtistas() {
+
     lArtistas->moverPrimero();
     while (!lArtistas->alFinal()) {
         delete lArtistas->consultar();
@@ -28,28 +33,26 @@ GestorArtistas::~GestorArtistas() { // [cite: 25]
     delete lArtistas;
 }
 
-void GestorArtistas::insertar(Artista* artista) { // [cite: 20, 21, 22]
+void GestorArtistas::insertar(Artista* artista) {
     lArtistas->moverPrimero();
     bool existe = false;
-
     while (!lArtistas->alFinal()) {
         Artista* a = lArtistas->consultar();
         if (a->get_nombre() == artista->get_nombre()) {
             existe = true;
-            break; // Sin duplicados [cite: 22]
+            break;
         }
         if (a->get_nombre() > artista->get_nombre()) {
-            break; // Inserción en orden alfabético [cite: 21]
+            break;
         }
         lArtistas->avanzar();
     }
-
     if (!existe) {
         lArtistas->insertar(artista);
     }
 }
 
-Artista* GestorArtistas::buscar(const string& nombre) const { // [cite: 23]
+Artista* GestorArtistas::buscar(const string& nombre) const {
     lArtistas->moverPrimero();
     while (!lArtistas->alFinal()) {
         Artista* a = lArtistas->consultar();
@@ -58,54 +61,195 @@ Artista* GestorArtistas::buscar(const string& nombre) const { // [cite: 23]
         }
         lArtistas->avanzar();
     }
+
     return nullptr;
 }
 
 void GestorArtistas::mostrar() const {
+
     lArtistas->moverPrimero();
+
     while (!lArtistas->alFinal()) {
-        lArtistas->consultar()->mostrar(); // Esto mostrara los artista y sus canciones
+
+        lArtistas->consultar()->mostrar();
+
         lArtistas->avanzar();
     }
 }
+
 int GestorArtistas::numElementos() const {
+
     int contador = 0;
+
     lArtistas->moverPrimero();
+
     while (!lArtistas->alFinal()) {
+
         contador++;
+
         lArtistas->avanzar();
     }
+
     return contador;
 }
+
 void GestorArtistas::mostrarTop() const {
 
     if (lArtistas->estaVacia()) {
-        cout << "No hay artistas en el sistema." << endl;
-        return;
+        cout << "No hay artistas." << endl;
+        return //como no hay artistas, acaba la función;
     }
 
-
     int maxSeguidores = 0;
-    Artista* artistaTop = nullptr;
-
-    // Recorremos la estructura para buscar el máximo
+    Artista* top = nullptr;
     lArtistas->moverPrimero();
+
     while (!lArtistas->alFinal()) {
+        
         Artista* a = lArtistas->consultar();
-
+        
         if (a->get_num_seguidores() > maxSeguidores) {
-            maxSeguidores = a->get_num_seguidores();
-            artistaTop = a;
+        maxSeguidores = a->get_num_seguidores();
+	    top = a;
         }
-
         lArtistas->avanzar();
     }
 
-    // Imprimimos el resultado
-    if (artistaTop) {
-        cout << "El artista mas seguidores es: " << artistaTop->get_nombre()
-             << " con " << maxSeguidores << " seguidores." << endl;
-    } else {
-        cout << "No se pudo determinar el artista top." << endl;
+    if (top != nullptr) {
+
+        cout << "El artista más top es: " << top->get_nombre() << " tiene " << maxSeguidores << " seguidores." << endl;
     }
 }
+
+#else
+
+
+// En caso de usarse por Árbol
+
+
+GestorArtistas::GestorArtistas() {
+    aArtistas = new BSTree<KeyValue<string, Artista*>>();
+}
+
+GestorArtistas::GestorArtistas(const GestorArtistas& otro) {
+    aArtistas = new BSTree<KeyValue<string, Artista*>>();
+    copiarRec(otro.aArtistas);
+}
+
+void GestorArtistas::copiarRec(BSTree<KeyValue<string, Artista*>>* nodo) {
+    if (nodo->estaVacio()) {
+        return;
+    }
+
+    KeyValue<string, Artista*> kv = nodo->getDato();
+    Artista* original = kv.getValue();
+    Artista* copia = new Artista(*original);
+
+    KeyValue<string, Artista*> nuevoKV(kv.getKey(),copia);
+    aArtistas->insertar(nuevoKV);
+    copiarRec(nodo->getIzq());
+    copiarRec(nodo->getDer());
+}
+
+
+GestorArtistas::~GestorArtistas() {
+    while (!aArtistas->estaVacio()) {
+        KeyValue<string, Artista*> kv = aArtistas->getDato();
+        Artista* a = kv.getValue();
+        aArtistas->eliminar(kv);
+        delete a;
+    }
+    delete aArtistas;
+}
+
+void GestorArtistas::insertar(Artista* artista) {
+
+    KeyValue<string, Artista*> clave(artista->get_nombre());
+
+    if (!aArtistas->existe(clave)) {
+        KeyValue<string, Artista*> kv(artista->get_nombre(),artista);
+        aArtistas->insertar(kv);
+    }
+}
+
+Artista* GestorArtistas::buscarArbrec(BSTree<KeyValue<string, Artista*>>* nodo, const string& nombre) const {
+
+    if (nodo->estaVacio()) {
+        return nullptr;
+    }
+
+    KeyValue<string, Artista*> kv = nodo->getDato();
+
+    if (kv.getKey() == nombre) {
+        return kv.getValue();
+    }
+
+    if (nombre < kv.getKey()) {
+        return buscarArbrec(nodo->getIzq(),nombre);
+    }
+    return buscarArbrec(nodo->getDer(),nombre);
+}
+
+Artista* GestorArtistas::buscar(const string& nombre) const {
+    return buscarArbrec(aArtistas, nombre);
+}
+
+void GestorArtistas::mostrarArbRec(BSTree<KeyValue<string, Artista*>>* nodo) const {
+    if (nodo->estaVacio()) {
+        return;
+    }
+
+    mostrarArbRec(nodo->getIzq());
+    nodo->getDato().getValue()->mostrar();
+    mostrarArbRec(nodo->getDer());
+}
+
+void GestorArtistas::mostrar() const {
+    
+    mostrarArbRec(aArtistas);
+}
+
+int GestorArtistas::contarArbolrecursivoYtal(BSTree<KeyValue<string, Artista*>>* nodo) const {
+    if (nodo->estaVacio()) {
+        return 0;
+    }
+
+    return 1 + contarArbolRecursivoYtal(nodo->getIzq()) + contarArbolRecursivoyTal(nodo->getDer());
+}
+
+int GestorArtistas::numElementos() const {
+    return contarArbolRecursivoYtal(aArtistas);
+}
+
+void GestorArtistas::topArblrec(BSTree<KeyValue<string, Artista*>>* nodo, Artista*& top, int& maxSeguidores) const {
+    if (nodo->estaVacio()) {
+        return;
+    }
+    
+    topArblrec(nodo->getIzq(), top, maxSeguidores);
+    Artista* actual = nodo->getDato().getValue();
+    if (actual->get_num_seguidores() > maxSeguidores) {
+
+        maxSeguidores = actual->get_num_seguidores();
+        top = actual;
+    }
+
+    topArblrec(nodo->getDer(), top, maxSeguidores);
+}
+
+void GestorArtistas::mostrarTop() const {
+    if (aArtistas->estaVacio()) {
+        cout << "No hay artistas top en esta lista." << endl;
+        return;
+    }
+    Artista* top = nullptr;
+    
+    int maxSeguidores = 0;
+    topArblrec(aArtistas, top, maxSeguidores);
+    
+    if (top != nullptr) {
+        cout << "Top artista: " << top->get_nombre() << " con " << maxSeguidores << " seguidores." << endl;
+    }
+}
+
+#endif
